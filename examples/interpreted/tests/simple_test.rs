7bfaf3ca796b10fd92e771f6f72f199b7b7334e5
Henrik Kvamme <henrik@kvamme.xyz>
2025-09-25 15:21:08 +0200

    Use Leo values with interpreter and add structs to interpreter bindings.

--- examples/interpreted/tests/simple_test.rs
+++ examples/interpreted/tests/simple_test.rs
@@ -13,4 +13,14 @@ fn dev() {
 
     let result = dev.main(&alice, 10u32, 5u32).unwrap();
     dbg!(result);
+
+    let a = A::new(1);
+    let b = B::new(2, a);
+    let result = dev.nested(&alice, b).unwrap();
+    dbg!(result);
+
+    let user = dev.create_user(&alice, alice.address(), 0, 0).unwrap();
+    dbg!(&user);
+    let balance = dev.consume_user(&alice, user).unwrap();
+    dbg!(balance);
 }
--- leo-bindings-core/src/generator_interpreter.rs
+++ leo-bindings-core/src/generator_interpreter.rs
@@ -79,15 +79,29 @@ pub fn generate_interpreter_code_from_simplified(
 
 
         fn leo_value_to_snarkvm_value(leo_value: leo_ast::interpreter_value::Value) -> Result<Value<Nw>, anyhow::Error> {
-            use leo_ast::interpreter_value::SvmValue;
-            let interpreter_svm_value: SvmValue = leo_value.try_into()
-                .map_err(|_| anyhow!("Failed to convert Leo value to SnarkVM value"))?;
-            Ok(interpreter_svm_value)
+            use leo_ast::interpreter_value::{SvmValue, ValueVariants};
+
+            match leo_value.contents {
+                ValueVariants::Svm(svm_value) => {
+                    Ok(svm_value)
+                },
+                ValueVariants::Unit => {
+                    Err(anyhow!("Cannot convert unit value to SnarkVM value"))
+                },
+                ValueVariants::Tuple(_) => {
+                    Err(anyhow!("Cannot convert Tuples"))
+                },
+                ValueVariants::Unsuffixed(_) => {
+                    Err(anyhow!("Cannot convert Unsuffixed literals"))
+                },
+                ValueVariants::Future(_) => {
+                    Err(anyhow!("Cannot convert Futures"))
+                }
+            }
         }
 
 
         pub struct #program_name {
-            pub package: Package,
             pub endpoint: String,
             pub interpreter: std::cell::RefCell<leo_interpreter::Interpreter>,
             session: SessionGlobals,
@@ -103,12 +117,12 @@ pub fn generate_interpreter_code_from_simplified(
                 let session = SessionGlobals::default();
                 let crate_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
 
-                let (package, interpreter) = SESSION_GLOBALS.set(&session, || {
+                let interpreter = SESSION_GLOBALS.set(&session, || {
                     #(#deployment_calls)*
 
                     let program_id = format!("{}.aleo", stringify!(#program_name));
 
-                    let mut package = Package::from_directory(
+                    let package = Package::from_directory(
                         crate_dir,
                         crate_dir,
                         false,
@@ -139,11 +153,10 @@ pub fn generate_interpreter_code_from_simplified(
 
                     interpreter.cursor.set_program(stringify!(#program_name));
 
-                    Ok::<_, anyhow::Error>((package, interpreter))
+                    Ok(interpreter)
                 })?;
 
                 Ok(Self {
-                    package,
                     endpoint: endpoint.to_string(),
                     interpreter: std::cell::RefCell::new(interpreter),
                     session,
@@ -161,7 +174,7 @@ pub fn generate_interpreter_code_from_simplified(
 
 fn generate_interpreter_function_implementations(
     functions: &[crate::signature::FunctionBinding],
-    _program_name: &str,
+    program_name: &str,
 ) -> Vec<proc_macro2::TokenStream> {
     functions.iter().map(|function| {
         let function_name = syn::Ident::new(&function.name, proc_macro2::Span::call_site());
@@ -215,7 +228,7 @@ fn generate_interpreter_function_implementations(
 
         let input_value_conversions: Vec<_> = function.inputs.iter().map(|input| {
             let param_name = syn::Ident::new(&input.name, proc_macro2::Span::call_site());
-            quote! { <_ as ToValue<Nw>>::to_value(&#param_name).to_string() }
+            quote! { snarkvm_value_to_leo_value(&<_ as ToValue<Nw>>::to_value(&#param_name))? }
         }).collect();
 
         quote! {
@@ -223,40 +236,68 @@ fn generate_interpreter_function_implementations(
                 let leo_result_value = SESSION_GLOBALS.set(&self.session, || {
                     let mut interpreter = self.interpreter.borrow_mut();
 
-                    let param_values = vec![#(#input_value_conversions),*];
-                    let function_call = format!("{}({})", stringify!(#function_name), param_values.join(", "));
+                    let param_values: Vec<leo_ast::interpreter_value::Value> = vec![#(#input_value_conversions),*];
 
-                    use leo_span::{source_map::FileName, with_session_globals};
-                    use leo_interpreter::{Frame, Element};
+                    let program_symbol = interpreter.cursor.current_program()
+                        .ok_or_else(|| anyhow!("No current program set in interpreter"))?;
+                    let function_name_symbol = leo_span::Symbol::intern(stringify!(#function_name));
 
-                    let filename = FileName::Custom(format!("user_input_{}", stringify!(#function_name)));
-                    let source_file = with_session_globals(|globals| globals.source_map.new_source(&function_call, filename));
+                    let param_count = param_values.len();
 
-                    let expression = leo_parser::parse_expression(
-                        leo_errors::Handler::default(),
-                        &interpreter.node_builder,
-                        &function_call,
-                        source_file.absolute_start,
-                        leo_ast::NetworkName::TestnetV0,
-                    ).map_err(|e| anyhow!("Failed to parse function call '{}': {}", function_call, e))?;
+                    for param_value in param_values {
+                        interpreter.cursor.values.push(param_value);
+                    }
 
-                    interpreter.cursor.frames.push(Frame {
-                        step: 0,
-                        element: Element::Expression(expression, None),
+                    let function_identifier = leo_ast::Identifier::new(function_name_symbol, interpreter.node_builder.next_id());
+                    let function_path = leo_ast::Path::new(
+                        vec![],
+                        function_identifier,
+                        None,
+                        leo_span::Span::default(),
+                        interpreter.node_builder.next_id(),
+                    );
+
+                    let call_expression = leo_ast::CallExpression {
+                        function: function_path,
+                        arguments: vec![leo_ast::Expression::Unit(leo_ast::UnitExpression {
+                            span: leo_span::Span::default(),
+                            id: interpreter.node_builder.next_id()
+                        }); param_count],
+                        const_arguments: vec![],
+                        program: Some(program_symbol),
+                        span: leo_span::Span::default(),
+                        id: interpreter.node_builder.next_id(),
+                    };
+
+                    interpreter.cursor.frames.push(leo_interpreter::Frame {
+                        step: 1,
+                        element: leo_interpreter::Element::Expression(
+                            leo_ast::Expression::Call(Box::new(call_expression)),
+                            None
+                        ),
                         user_initiated: true,
                     });
 
-                    let step_result = interpreter.cursor.over()
-                        .map_err(|e| anyhow!("Failed to execute function '{}': {}", stringify!(#function_name), e))?;
+                    let mut result = None;
+                    loop {
+                        let step_result = interpreter.cursor.whole_step()
+                            .map_err(|e| anyhow!("Failed to execute function '{}': {}", stringify!(#function_name), e))?;
 
-                    let result = if step_result.finished { step_result.value } else { None };
+                        if step_result.finished {
+                            result = step_result.value;
+                            break;
+                        }
+
+                        if interpreter.cursor.frames.is_empty() {
+                            break;
+                        }
+                    }
 
                     let function_outputs: Vec<snarkvm::prelude::Value<Nw>> = match result {
                         Some(leo_value) => {
-                            match leo_value.contents {
-                                leo_ast::interpreter_value::ValueVariants::Svm(svm_value) => vec![svm_value],
-                                leo_ast::interpreter_value::ValueVariants::Unit => vec![],
-                                _ => return Err(anyhow!("Unsupported return value type: {:?}", leo_value.contents)),
+                            match leo_value_to_snarkvm_value(leo_value) {
+                                Ok(svm_value) => vec![svm_value],
+                                Err(e) => return Err(anyhow!("Failed to convert Leo return value to SnarkVM: {}", e)),
                             }
                         },
                         None => vec![],
